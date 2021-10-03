af977ede6d6a36378594a09841c763bbbfa7a4d7
mocup <83974973+mocup@users.noreply.github.com>
2021-10-03 16:10:27 -0400

    Create utils_extern.h

---
+++ utils_extern.h
@@ -0,0 +1,16 @@
+#ifndef __UTILS_EXTERN_H__
+#define __UTILS_EXTERN_H__
+
+void LED_ExInit(void);
+void White_On(void);
+void White_Off(void);
+void Yellow_On(void);
+void Yellow_Off(void);
+void Red_On(void);
+void Red_Off(void);
+void Blue_On(void);
+void Blue_Off(void);
+void Green_On(void);
+void Green_Off(void);
+void Button_Init(void);
+#endif
